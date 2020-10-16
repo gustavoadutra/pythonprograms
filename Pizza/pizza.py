13a6a905e862c7d492122ebb5700f8bcbeb5180d
gustavoadutra <gustavoarthurdutra@protonmail.com>
2020-10-16 12:38:45 -0300

    Mp3PlayerAlvinEosEsquilos

--- Pizza/pizza.py
+++ Pizza/pizza.py
@@ -1,22 +1,33 @@
 #Mostra as musicas em uma pasta
-#Você pode selecionar qual musica quer tocar..
+#Você pode selecionar qual musica quer tocar.
 import os
-dic_musica = {}
-def org_music(dire_musica):
-    lista_musicas = os.listdir(dire_musica)
+import pyglet
+
+def menu_music(dire_musica):
+    c = 0
+    dic_musica = {}
+    lista_musicas = os.listdir(dire_musica)    
     for arquivo in lista_musicas:
         if arquivo.endswith('.mp3'):
-            for c in range(0, len(lista_musicas)):
-                dic_musica[c] = arquivo    
-    print(dic_musica)
-    return dic_musica
+            c += 1
+            dic_musica[c] = arquivo
+    escolha_fin = sele_music(dic_musica)
+    iniciar_music(dire_musica, escolha_fin)
+    
+
+def sele_music(dic_musica):
+    for arquivo in dic_musica:
+        print(f'({arquivo}) {dic_musica[arquivo]}')
+    escolha = int(input('Qual música?'))
+    escolha_fin = dic_musica[escolha]
+    return escolha_fin
 
 
-def menu(dir_musica, dic_musica):
-    print('MENUM')
-    for arquivo, c in enumerate(dic_musica):
-        print(arquivo, c)
+def iniciar_music(dire_musica, escolha_fin):
+    musica_dir = f'{dire_musica}/{escolha_fin}'
+    music = pyglet.media.load(musica_dir)
+    music.play()
+    pyglet.app.run()
 
-dire_musica = 
-dic_musica = org_music(dire_musica)
-menu('/home/gustavoad/Documents/Certificados/Musicas', )
+dire_musica = '/home/gustavoad/Documents/Certificados/Musicas'
+menu_music(dire_musica)
